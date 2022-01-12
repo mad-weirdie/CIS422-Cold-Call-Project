e013c54064dba42dc54cf2bedec519a4970db7bc
mad-weirdie <mwerries@uoregon.edu>
2022-01-12 15:26:59 -0800

    Started functions for student queue!
    TODO: figure out which format we're using to save the queue directly to a file and load from that file again. the student queue class is assuming that the main driver or another module is responsible for checking which place we should load the queue from (either a brand new roster or an old queue), and is responsible for providing functions to do either one.

--- app/student_queue.py
+++ app/student_queue.py
@@ -1,14 +1,75 @@
 #!/usr/bin/env python3
-
+from student import *
+import random
 """
 The core structure and functionality of the Student Queue.
 """
-
-from student import *
+# Global variable for the number of students the instructor would like to have
+# "on deck" from the queue at any given time.
+num_on_deck = 4
+# Don't insert students back into the queue at a certain percentage of the head.
+insert_delay = 0.2
 
 class StudentQueue:
-	queue = []
-	queue_size = 0
-	num_on_deck = 0
+	student_queue = []
 
+	""" Basic constructor for the student queue. """
+	def __init__(self):
+		student_queue = []
 
+	""" Fills the queue using data from an instance of the roster class. """
+	def queue_from_roster(self, roster):
+		for student in roster:
+			self.student_queue.append(student)
+		# Randomize the queue order
+		self.shuffle_queue()
+
+	def save_queue_to_file(self, filename):
+		# TODO: FIGURE OUT EXACTLY WHAT FORMAT TO STORE THE QUEUE IN
+		f = open(filename, "w")
+		for student in self.student_queue:
+			f.write(student)
+
+	""" Fills the queue using saved queue data from a file. """
+	def load_queue_from_file(self, queue):
+		# TODO: FIGURE OUT EXACTLY WHAT FORMAT TO STORE THE QUEUE IN
+		for student in queue:
+			# Maintain the ordering of the queue
+			self.student_queue.append(student)
+
+	def get_on_deck(self):
+		# Check we have enough students in the roster to place "on deck."
+		# TODO: ALTERNATIVE: only 1-3 students in the roster means they're on deck forever?
+		if self.queue_size() < num_on_deck:
+			raise ValueError("On-deck size is greater than size of queue.")
+		start = self.queue_size() - num_on_deck
+		stop = self.queue_size()
+		on_deck = []
+		for i in range(start, stop):
+			on_deck.append(self.student_queue[i])
+		assert(len(on_deck) == num_on_deck)
+		return on_deck
+
+	""" Randomly shuffles all the students in the queue. """
+	def shuffle_queue(self):
+		for student in self.student_queue:
+			self.student_queue.remove(student)
+			rand_index = random.randint(0, self.queue_size())
+			self.student_queue.insert(rand_index, student)
+
+	"""
+	Insert student into random position in the queue, but only up to a
+	certain position relative to the queue size and the insert_delay factor.
+	"""
+	def randomized_enqueue(self, student):
+		stop = int(self.queue_size() * (1-insert_delay))
+		rand_index = random.randint(0, stop)
+		self.student_queue.insert(rand_index, student)
+
+	""" Remove a specific student from the queue. """
+	def dequeue_student(self, student):
+		self.student_queue.remove(student)
+
+	""" Returns the current size of the student queue. """
+	def queue_size(self):
+		return len(self.student_queue)
