fb527fda146e5cb50eb7133b1f33cb2c431d8861
Derek Martin <derekmartinnj@gmail.com>
2022-01-16 16:50:16 -0800

    Random Dist Verfication update
    - building skeleton code
    - still need to write to output file
    - still need to specify which roster file to import

--- app/random_distribution_verification.py
+++ app/random_distribution_verification.py
@@ -16,19 +16,21 @@ class RandomVerification:
     #   Overwrite this file each time the instructor enters Random Distribution Verification Mode
 
     ##############################################################################################################
+
     def __init__(self, controller):
         self.key_sequence = key_sequence.KeySequence()
         self.controller = controller
+
+    ##############################################################################################################
 
     def add_and_check_for_random_verification(self, new_key):
         """ Will get called by the controller every time a key is pressed."""
         self.key_sequence.add_key(new_key)
         if self.key_sequence.check_for_match():
-            print("Match! do random verification mode")
             self.start()
             self.key_sequence.reset()
-        else:
-            print("No match")
+
+    ##############################################################################################################
 
     def start(self):
         do_random_verification = messagebox.askokcancel(
@@ -36,6 +38,7 @@ class RandomVerification:
         )
         if do_random_verification:
             self.create_output_file()
+            self.create_test_queue()
             self.run()
     
     ##############################################################################################################
@@ -45,39 +48,42 @@ class RandomVerification:
 
     ##############################################################################################################
 
-    def restart_app(self):
-        controller = Controller()
+    def create_test_queue(self):
+        #   Should the test queue be created from an instance of Roster class, or imported from a file?
+        #   NOTE:   After running Rand Dist Verficiation, we probably want the queue to be how it was before.
+        #           So, either make a copy of the queue from Roster class, or import a new test queue from Pickle file.
+
+        self.test_queue = StudentQueue()
+        self.test_queue.queue_from_roster(roster)   #TODO: specify the roster file to import
 
     ##############################################################################################################
 
-    def generate_hundred_calls(self):
+    def restart_app(self):
+        # call randomization function from student_queue to simulate an application restart
+        self.test_queue.shuffle_front_and_back()
+
+    ##############################################################################################################
+
+    def generate_hundred_cold_calls(self):
         for i in range(100):
             # call the controller file here
-            
-
-            result = "" # format the cold call info into a string?
+            self.test_queue.take_off_deck(student)  #TODO: Determine which student to dequeue
+            result = student                        #TODO: Figure out what to write to output file
             self.write_to_output_file(result)
-
 
     ##############################################################################################################
 
     def write_to_output_file(self, result):
         self.out.write(result + "\n")
-        pass
 
     ##############################################################################################################
 
     def run(self):
         for i in range(100):
             self.restart_app()
-            self.generate_hundred_calls()
+            self.generate_hundred_cold_calls()
 
 ##################################################################################################################
             
 RDV = RandomVerification()
 RDV.start()
-
-
-"""
-
-"""
