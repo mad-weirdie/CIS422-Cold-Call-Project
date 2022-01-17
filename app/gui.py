d47f60e0e3de1763933a3a792e985ca9484f5e2e
Quinn <91642013+qfetrow@users.noreply.github.com>
2022-01-16 17:03:03 -0800

    Big Bar!

--- app/gui.py
+++ app/gui.py
@@ -10,7 +10,7 @@ class Display:
         self.main_window = Tk()
         self.main_window.configure(bg="white")
         self.main_window.title("Cold Call application")
-        self.main_window.geometry('900x80')
+        self.main_window.geometry(f'{self.main_window.winfo_screenwidth()}x70')
         self.main_window.resizable(False, False)
 
         self.main_window.bind_all("<Left>", controller.shift_index_left)
@@ -41,8 +41,9 @@ class Display:
         for student in on_deck:
             names.append(student.get_name())
 
+        self.main_window.columnconfigure(0, minsize=self.main_window.winfo_screenwidth()/7)
         for i in range(self.NUM_ON_DECK):
-            self.main_window.columnconfigure(i+1, minsize=140)
+            self.main_window.columnconfigure(i+1, minsize=self.main_window.winfo_screenwidth()/6)
         
         label = Label(self.main_window, bg="white", fg="black",text="Next students:", width=0)
         self.main_window.attributes('-topmost', True)
@@ -57,7 +58,7 @@ class Display:
             self.labels[n].configure(bg=bg_color)
             self.labels[n].configure(fg=fg_color)
             self.labels[n].configure(text=names[n])
-            self.labels[n].grid(row=0, column=n+1, sticky="W", rowspan=1)
+            self.labels[n].grid(row=0, column=(n+1), sticky="W", rowspan=1)
 
-        self.import_button.grid(row=0, column=5, columnspan=1, padx=3)
-        self.export_button.grid(row=0, column=6, columnspan=1, padx=3)
+        self.import_button.grid(row=0, column=(self.NUM_ON_DECK+1), columnspan=1, padx=20)
+        self.export_button.grid(row=0, column=(self.NUM_ON_DECK+2), columnspan=1, padx=3)
