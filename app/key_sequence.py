8d72b816aae1d95a05c242ec2be781c7218adde8
Derek Martin <derekmartinnj@gmail.com>
2022-01-30 12:37:24 -0800

    working on documentation

--- app/key_sequence.py
+++ app/key_sequence.py
@@ -21,22 +21,29 @@ Last Edit By:   Arden Butterfield
 class KeySequence:
     """
     A class to store and check the most recent sequence of key presses.
+
     Attributes
     ============================================================================
     target_sequence:
-        A list of the pattern of keys, in sequence, that we need to press.
+        A list of the pattern of keys, in sequence, that we need to press in order
+        to enter Random Distribution Verification mode.
+
     key_sequence:
         A list of the <n> most recent keys pressed, where <n> is the length of
         the target_sequence.
+
     The names of the keys here are the <keysym>s defined by Tkinter. They are
     stored as strings.
+
     Methods
     ============================================================================
     add_key(key)
-        Add the most recently pressed key to the key sequence. This should be
+        Add the most recently pressed key to the key sequence. This is
         called after every key press.
+
     check_for_match()
         Does the recent sequence of key presses match the target sequence?
+
     reset()
         Remove all keystrokes from the key sequence.
     """
@@ -49,10 +56,10 @@ class KeySequence:
 
     def add_key(self, key):
         """
-        Add the most recently pressed key to the key sequence. This should be
+        Add the most recently pressed key to the key sequence. This is
         called after every key press.
 
-        key: the keysym string of the most recent key pressed.
+        key: (string) the keysym string of the most recent key pressed.
         """
         self.key_sequence.append(key)
         if len(self.key_sequence) > len(self.target_sequence):
@@ -63,7 +70,7 @@ class KeySequence:
     def check_for_match(self):
         """
         Does the recent sequence of key presses match the target sequence?
-        Returns: true if it does match, false if it does not.
+        Returns: (boolean) True if it does match, False if it does not.
         """
         return self.key_sequence == self.target_sequence
 
